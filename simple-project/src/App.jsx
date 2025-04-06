ba58cc2d5148b734daf8bb805c65de0ac28e3fbe
Moshiur-Rahman-01 <moshiur.ict24@gmail.com>
2025-04-06 12:57:46 +0600

    added

--- simple-project/src/App.jsx
+++ simple-project/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
@@ -6,8 +7,22 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
+
+      <div className="flex gap-2 m-1">
+        {/* left side */}
+        <div className="w-3/4 border-2 rounded">
+          
+          <Blogs></Blogs>
+        </div>
+
+        {/* right side */}
+        <div className="w-1/4 border-2 rounded">
+          <h2>Reading time: 0</h2>
+          <h2>Bookmark count: 0</h2>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default App
